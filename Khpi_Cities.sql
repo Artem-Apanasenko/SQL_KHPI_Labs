f9ee0cbb3fc746cbf8779844384f93e63eb63bb9
Artem_Apanasenko <artemi.apanas@gmail.com>
2023-09-17 15:56:20 +0200

    Lab_n2

--- Khpi_Cities.sql
+++ Khpi_Cities.sql
@@ -64,6 +64,8 @@ INSERT INTO `cities` (`id`, `name`, `population`, `region`) VALUES
 (49,	'Кадіївка',	92132,	'E'),
 (50,	'Конотоп',	92000,	'E');
 
+# LAB1
+
 SELECT t.*
 FROM khpi_cities.cities t
 ORDER BY population DESC
@@ -78,9 +80,34 @@ SELECT t.*
 FROM khpi_cities.cities t
 ORDER BY region, population DESC
 
-SELECT region
-FROM khpi_cities.cities t
+# LAB2
 
-SELECT name, region
+SELECT name
 FROM khpi_cities.cities t
-ORDER BY region DESC, name DESC
+WHERE population >=1000000
+
+SELECT region, population
+FROM khpi_cities.cities t
+WHERE region='W' OR region='E'
+ORDER BY population DESC
+
+SELECT region, name, population
+FROM khpi_cities.cities t
+WHERE population >=50000 AND region IN('S','C','N')
+ORDER BY population DESC
+
+SELECT region, name, population
+FROM khpi_cities.cities t
+WHERE (population>=150000 AND population<=350000) AND region IN('E','W','N')
+ORDER BY name ASC
+LIMIT 20
+
+SELECT name, region, population, id
+FROM khpi_cities.cities t
+WHERE NOT region IN('W','E')
+ORDER BY population DESC
+LIMIT 10
+OFFSET 10 # =LIMIT 10,10
+
+
+
