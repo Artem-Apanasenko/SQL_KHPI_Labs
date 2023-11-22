7914bb203143b06c14def165cc1e637b706a57c7
Artem-Apanasenko <116758922+Artem-Apanasenko@users.noreply.github.com>
2023-11-22 01:06:16 +0100

    Separate other labs from Khpi_Cities.sql

--- Khpi_Cities.sql
+++ Khpi_Cities.sql
@@ -80,34 +80,5 @@ SELECT t.*
 FROM khpi_cities.cities t
 ORDER BY region, population DESC
 
-# LAB2
-
-SELECT name
-FROM khpi_cities.cities t
-WHERE population >=1000000
-
-SELECT region, population
-FROM khpi_cities.cities t
-WHERE region='W' OR region='E'
-ORDER BY population DESC
-
-SELECT region, name, population
-FROM khpi_cities.cities t
-WHERE population >=50000 AND region IN('S','C','N')
-ORDER BY population DESC
-
-SELECT region, name, population
-FROM khpi_cities.cities t
-WHERE (population>=150000 AND population<=350000) AND region IN('E','W','N')
-ORDER BY name ASC
-LIMIT 20
-
-SELECT name, region, population, id
-FROM khpi_cities.cities t
-WHERE NOT region IN('W','E')
-ORDER BY population DESC
-LIMIT 10
-OFFSET 10 # =LIMIT 10,10
-
 
 
